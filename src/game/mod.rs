605d33e3c104f8240c6670bdb6a63ff235b1f0ab
lee-orr <lee-orr@users.noreply.github.com>
2023-01-02 18:15:18 -0500

    fix wasm build

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,14 +1,16 @@
 mod board;
-mod editor_ui;
 mod game_state;
 pub mod level;
 mod turn_start_ui;
+
+#[cfg(not(target_family = "wasm"))]
+mod editor_ui;
 
 use bevy::prelude::*;
 use iyes_loopless::prelude::AppLooplessStateExt;
 
 use self::{
-    board::BoardPlugin, editor_ui::EditorUiPlugin, game_state::GameState,
+    board::BoardPlugin,  game_state::GameState,
     turn_start_ui::TurnStartUiPlugin,
 };
 
@@ -18,7 +20,9 @@ impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
         app.add_loopless_state(GameState::Setup)
             .add_plugin(BoardPlugin)
-            .add_plugin(TurnStartUiPlugin)
-            .add_plugin(EditorUiPlugin);
+            .add_plugin(TurnStartUiPlugin);
+            #[cfg(not(target_family = "wasm"))]
+        app
+            .add_plugin(self::editor_ui::EditorUiPlugin);
     }
 }
